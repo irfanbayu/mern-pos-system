a758b1684a48cf393f36c7ea8e3cd3da10d6e4af
irfanbayu <setiawan.irfanbayu@gmail.com>
2022-04-10 23:20:52 +0700

    add filter and count

--- eduwork-server/app/product/controller.js
+++ eduwork-server/app/product/controller.js
@@ -88,13 +88,53 @@ const store = async (req, res, next) => {
 
 const index = async (req, res, next) => {
   try {
-    let { skip = 0, limit = 10 } = req.query;
-    let product = await Product.find()
+    let { skip = 0, limit = 10, q = "", category = "", tags = [] } = req.query;
+
+    let criteria = {};
+
+    //untuk filter products
+    if (q.length) {
+      criteria = {
+        ...criteria,
+        name: { $regex: `${q}`, $options: "i" },
+      };
+    }
+
+    if (category.length) {
+      let categoryResult = await Category.findOne({
+        name: { $regex: `${category}`, $options: "i" },
+      });
+      if (categoryResult) {
+        criteria = {
+          ...criteria,
+          category: categoryResult._id,
+        };
+      }
+
+      if (tags.length) {
+        let tagsResult = await Tags.find({
+          name: { $in: tags },
+        });
+        if (tagsResult.length > 0) {
+          criteria = {
+            ...criteria,
+            tags: { $in: tagsResult.map((tags) => tags._id) },
+          };
+        }
+      }
+    }
+
+    // console.log(criteria);
+    let count = await Product.find().countDocuments();
+    let product = await Product.find(criteria)
       .skip(parseInt(skip))
       .limit(parseInt(limit))
       .populate("category")
       .populate("tags");
-    return res.json(product);
+    return res.json({
+      data: product,
+      count,
+    });
   } catch (err) {
     next(err);
   }
