b115c183e38d2ffdd8db509fa541184a0d7f895d
irfanbayu <setiawan.irfanbayu@gmail.com>
2022-04-12 16:42:23 +0700

    fix component delivery address

--- eduwork-server/app/deliveryaddress/controller.js
+++ eduwork-server/app/deliveryaddress/controller.js
@@ -1,3 +1,6 @@
+const { subject } = require("@casl/ability");
+const { policy_check } = require("../../middleware");
+const { policyFor } = require("../../utils");
 const DeiveryAddress = require("./model");
 
 const store = async (req, res, next) => {
@@ -21,15 +24,25 @@ const store = async (req, res, next) => {
 
 const update = async (req, res, next) => {
   try {
-    let payload = req.body;
-    let address = await DeiveryAddress.findByIdAndUpdate(
-      req.params.id,
-      payload,
-      {
-        new: true,
-        runValidators: true,
-      }
-    );
+    let { _id, ...payload } = req.body;
+    let { id } = req.params;
+    let address = await DeiveryAddress.findById(id);
+    let subjectAddress = subject("DeliveryAddress", {
+      ...address,
+      user_id: address.user,
+    });
+    let policy = policyFor(req.user);
+    if (!policy.can("update", subjectAddress)) {
+      return res.json({
+        error: 1,
+        message: "You don't have permission to update this address",
+      });
+    }
+
+    address = await DeiveryAddress.findByIdAndUpdate(id, payload, {
+      new: true,
+      runValidators: true,
+    });
     return res.json(address);
   } catch (err) {
     if (err && err.name === "ValidationError") {
@@ -45,8 +58,25 @@ const update = async (req, res, next) => {
 
 const destroy = async (req, res, next) => {
   try {
-    let address = await DeiveryAddress.findByIdAndDelete(req.params.id);
-    return res.json(address);
+    let { id } = req.params;
+    let address = await DeiveryAddress.findById(id);
+    let policy = policyFor(req.user);
+    let subjectAddress = subject("DeliveryAddress", {
+      ...address,
+      user_id: address.user,
+    });
+    if (!policy.can("delete", subjectAddress)) {
+      return res.json({
+        error: 1,
+        message: "You don't have permission to delete this address",
+      });
+    }
+    address = await DeiveryAddress.findByIdAndDelete(id);
+    // return res.json({
+    // error: 0,
+    // message: "Address deleted successfully",
+    // });
+    res.json(address);
   } catch (err) {
     if (err && err.name === "ValidationError") {
       return res.json({
@@ -61,8 +91,15 @@ const destroy = async (req, res, next) => {
 
 const index = async (req, res, next) => {
   try {
-    let address = await DeiveryAddress.find();
-    return res.json(address);
+    let { skip = 0, limit = 10 } = req.query;
+    let count = await DeiveryAddress.find({
+      user: req.user._id,
+    }).countDocuments();
+    let address = await DeiveryAddress.find({ user: req.user._id })
+      .skip(parseInt(skip))
+      .limit(parseInt(limit))
+      .sort("-createdAt");
+    return res.json({ data: address, count });
   } catch (err) {
     if (err && err.name === "ValidationError") {
       return res.json({
